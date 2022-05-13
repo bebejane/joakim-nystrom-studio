05f28e56212c4b151cccfbd0b162d0f550b610d5
Bebe Jane <bjornthief@gmail.com>
2022-05-13 18:58:30 +0200

    add buy subject mailto

--- components/Artwork.js
+++ components/Artwork.js
@@ -1,50 +1,36 @@
 import styles from './Artwork.module.scss';
 import cn from 'classnames'
 import { Image } from 'react-datocms';
-import { GetAllArtwork } from '/graphql';
 import Content from './Content';
 import ArtworkGallery from './ArtworkGallery';
-import { motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-
-const duration = 0.4;
-const variants =  { 
-	initial:{
-		opacity:0
-	},
-	fromIndex:{
-		translateY:['-100vh', '0vh'],
-		transition:{ease:'easeOut', duration}
-	},
-	fromStudio:{
-		opacity:1,
-		transition:{ease:'easeOut', duration}
-	},
-	toIndex:{
-		translateY:'-100%',
-		transition:{ease:'linear', duration}
-	},
-	toStudio:{
-		opacity:0,
-		transition:{ease:'easeOut', duration}
-	}
-}
 
 export default function Artwork({artwork, onShowGallery, prevRoute}){	
 
 	const [galleryIndex, setGalleryIndex] = useState()
+	
 	useEffect(()=>{ onShowGallery?.(galleryIndex !== undefined) }, [galleryIndex])
 
 	return (
 		<>
 			<Content className={styles.artwork}>
 				<ul>
-					{artwork.map(({image, dimensions, sold}, idx) => 
+					{artwork.map(({image, dimensions, sold, title}, idx) => 
 						<li key={idx} onClick={()=>setGalleryIndex(idx)}>
 							<Image data={image.responsiveImage} className={styles.image} pictureClassName={styles.picture}/>
 							<span className={styles.dimensions}>{dimensions}</span>
-							<span className={cn(styles.availability, sold && styles.sold)}>{sold ? 'Sold' : 'Buy'}</span>
+							<span className={cn(styles.availability, sold && styles.sold)}>
+								{sold ? 
+									<>Sold</>
+								: 
+									<a 
+										href={`mailto:info@joakimnystrom.com${ title ? `?subject=${encodeURIComponent(title)}` : ''}`} 
+										onClick={e => e.stopPropagation()}
+									>
+										Buy
+									</a>
+								}
+							</span>
 						</li>
 					)}
 				</ul>
