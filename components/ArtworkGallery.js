0bef330024a0556fa1009f37404779db9e947e00
Bebe Jane <bjornthief@gmail.com>
2022-06-03 16:30:42 +0200

    add revalidate

--- components/ArtworkGallery.js
+++ components/ArtworkGallery.js
@@ -4,15 +4,14 @@ import { Image } from "react-datocms"
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { useState, useRef, useEffect } from 'react';
 
-export default function ArtworkGallery({ images, onClose, index = 0 }) {
-
+export default function ArtworkGallery({ artwork, onClose, index = 0 }) {
+  
   const swiperRef = useRef()
   const [realIndex, setRealIndex] = useState(index)
   const [title, setTitle] = useState()
+  const current = artwork[realIndex]
 
-  useEffect(() => setTitle(images[realIndex].title), [realIndex])
-
-  if (!images) return null
+  useEffect(() => setTitle(artwork[realIndex].title), [realIndex])
 
   return (
     <div className={styles.artworkGallery}>
@@ -26,7 +25,7 @@ export default function ArtworkGallery({ images, onClose, index = 0 }) {
           onSlideChange={({ realIndex }) => setRealIndex(realIndex)}
           onSwiper={(swiper) => swiperRef.current = swiper}
         >
-          {images.map((image, idx) =>
+          {artwork.map(({image}, idx) =>
             <SwiperSlide key={idx} className={styles.slide}>
               {<Image
                 className={styles.image}
@@ -41,10 +40,23 @@ export default function ArtworkGallery({ images, onClose, index = 0 }) {
       </div>
       <div className={styles.forward} onClick={() => swiperRef.current.slideNext()}>→</div>
       <div className={styles.caption}>
-        Edition: 1<br />
-        Size: 45x34cm<br />
-        Prize: 200 Euro<br />
-        Buy<br />
+        {current && 
+          <>
+            Edition: {current.edition}<br />
+            Size: {current.dimensions}<br />
+            Prize: {current.price}<br />
+            {current.sold ?
+              <>Sold</>
+            :
+              <a
+                href={`mailto:info@joakimnystrom.com${current.title ? `?subject=${encodeURIComponent(current.title)}` : ''}`}
+                onClick={e => e.stopPropagation()}
+              >
+                Buy
+              </a>
+            }
+          </>
+        }
       </div>
       <div className={styles.close} onClick={onClose}>×</div>
     </div >
