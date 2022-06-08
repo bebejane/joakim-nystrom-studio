b55ec40b0ad230efc709386a5cc062a135cc195f
Bebe Jane <bjornthief@gmail.com>
2022-06-08 10:32:50 +0200

    add sold thumbnails

--- components/Artwork.js
+++ components/Artwork.js
@@ -19,7 +19,12 @@ export default function Artwork({ artwork, onShowGallery, onIndexChange, prevRou
 				<ul>
 					{artwork.map(({ image, dimensions, sold, title }, idx) =>
 						<li key={idx} onClick={() => setGalleryIndex(idx)}>
-							<Image data={image.responsiveImage} className={styles.image} pictureClassName={styles.picture} />
+							<Image 
+								data={image.responsiveImage} 
+								className={styles.image} 
+								pictureClassName={styles.picture}
+							/>
+							{sold && <span className={styles.sold}>Sold</span>}
 						</li>
 					)}
 				</ul>
